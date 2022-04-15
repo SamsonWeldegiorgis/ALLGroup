7a396c29304f6c6d498296b67566bcb78ae0e9b9
Samson Weldegiorgis <S.WELDEGIORGIS7347@edmail.edcc.edu>
2022-04-14 19:21:40 -0700

    Develpoing more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println("Work1 started");
         System.out.println("Created Git Repotitory");
         System.out.println("Made few commits");
+        System.out.println("Developing hello feature");
     }
 }
